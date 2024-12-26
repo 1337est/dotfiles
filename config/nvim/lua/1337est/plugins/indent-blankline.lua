d306bcb953e3e76cb4f7a58e4c58a40d90a0f8aa
1337est <59178218+1337est@users.noreply.github.com>
2024-12-26 13:01:02 -0600

    archiving files and integrating conform and nvim-cmp into lsp.lua

--- config/nvim/lua/1337est/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- Adds indentation guides between blocks of code
-	-- e.g. 2 indentation guide to the left
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	opts = {
-		-- e.g. 3 indentation guides to the left
-	},
-}
