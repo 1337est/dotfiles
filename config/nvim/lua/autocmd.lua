5028844512484a273f42b3fd525dab6b8ee40ca5
1337est <59178218+1337est@users.noreply.github.com>
2024-12-29 15:20:25 -0600

    Updating lazy.nvim config with defaults

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -27,9 +27,9 @@ autocmd({ "BufWritePre" }, {
 vim.api.nvim_command([[ highlight MatchParen guibg=#0030F0 ]])
 
 -- Enables wrap text for Markdown and Text files
-vim.cmd([[autocmd BufEnter *.md,*.txt set wrap]])
+vim.cmd([[autocmd BufEnter *.md,*.txt,*.norg set wrap]])
 -- Enable spell checking for Markdown and Text files
-vim.cmd([[autocmd BufEnter *.md,*.txt set spell]])
+vim.cmd([[autocmd BufEnter *.md,*.txt,*.norg set spell]])
 
 -- Getting rid of auto-inserted comments
 vim.cmd([[autocmd BufEnter * set formatoptions-=cro ]])
