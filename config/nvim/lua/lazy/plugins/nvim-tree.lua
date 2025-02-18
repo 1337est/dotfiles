5c9b37c96d5a74574280b7260c6bc1ac42b642f5
1337est <59178218+1337est@users.noreply.github.com>
2025-02-18 12:56:17 -0600

    Added some custom keymaps for nvim-tree

--- config/nvim/lua/lazy/plugins/nvim-tree.lua
+++ config/nvim/lua/lazy/plugins/nvim-tree.lua
@@ -9,11 +9,33 @@ return {
         -- disable netrw
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-        require("nvim-tree").setup {}
+        local api = require "nvim-tree.api"
+
+        -- setup on_attach settings
+        local function my_on_attach(bufnr)
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            -- default mappings
+            api.config.mappings.default_on_attach(bufnr)
+
+            -- custom mappings
+            vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+        end
+
+        -- pass to setup along with your other options
+        require("nvim-tree").setup {
+            ---
+            on_attach = my_on_attach,
+            ---
+        }
 
         -- to prevent statusline from showing for NvimTree
-        require("nvim-tree.api").events.subscribe("TreeOpen", function()
+        api.events.subscribe("TreeOpen", function()
             vim.wo.statusline = ' '
         end)
+        vim.keymap.set("n", "<leader>tt", api.tree.toggle,
+            { desc = "nvim-tree: [T]oggle [T]ree", noremap = true, silent = true, nowait = true })
     end,
 }
