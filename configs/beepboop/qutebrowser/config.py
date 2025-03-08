26dbd8917d1999125bb289fc48b9b6401b6b1e79
1337est <59178218+1337est@users.noreply.github.com>
2025-03-08 14:41:35 -0600

    FIXED: Debugging to fix fonts in qutebrowser

--- configs/beepboop/qutebrowser/config.py
+++ configs/beepboop/qutebrowser/config.py
@@ -1,34 +1,20 @@
 config.load_autoconfig(False)
 
 config.source('colors.py')
+config.source('fonts.py')
 
 c.completion.height = "25%"
 c.completion.scrollbar.padding = 2
 c.completion.scrollbar.width = 16
 
 c.content.pdfjs = True
+c.content.user_stylesheets = ['~/.config/qutebrowser/stylesheet.css']
 
 c.editor.command = ["ghostty", "-e", "nvim {file}"]
 
 c.fileselect.folder.command = ["ghostty", "-e", "yazi", "--cwd-file={}"]
 c.fileselect.multiple_files.command = ["ghostty", "-e", "yazi", "--chooser-file={}"]
 c.fileselect.single_file.command = ["ghostty", "-e", "yazi", "--chooser-file={}"]
-
-c.fonts.contextmenu = "default_size default_family"
-c.fonts.default_family = ["JetBrainsMono Nerd Font", "monospace"]
-c.fonts.default_size = "12pt"
-c.fonts.prompts = "default_size default_family"
-c.fonts.tooltip = "default_size default_family"
-c.fonts.web.family.cursive = "default_family"
-c.fonts.web.family.fantasy = "default_family"
-c.fonts.web.family.fixed = "default_family"
-c.fonts.web.family.sans_serif = "default_family"
-c.fonts.web.family.serif = "default_family"
-c.fonts.web.family.standard = "default_family"
-c.fonts.web.size.default = 16
-c.fonts.web.size.default_fixed = 16
-c.fonts.web.size.minimum = 12
-c.fonts.web.size.minimum_logical = 12
 
 c.hints.find_implementation = "javascript"
 
