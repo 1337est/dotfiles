e14571f8305c3db2d96f5c048f73ab9c5c97665b
1337est <59178218+1337est@users.noreply.github.com>
2025-06-17 05:30:00 -0500

    Updating neovim visual color line

--- configs/confighome/shellements/nvim/lua/1337/set.lua
+++ configs/confighome/shellements/nvim/lua/1337/set.lua
@@ -59,9 +59,9 @@ vim.opt.splitbelow = true
 -- Preview substitutions live as you type
 vim.opt.inccommand = "split"
 
-vim.opt.colorcolumn = "81" -- Line at the nth column
+vim.opt.colorcolumn = "101" -- Line at the nth column
 
-vim.opt.mouse = "a"        -- Enables mouse mode
+vim.opt.mouse = "a"         -- Enables mouse mode
 vim.opt.mousescroll = "ver:1,hor:2"
 
 vim.opt.showmode = false -- Don't show mode, since it's already in status line
