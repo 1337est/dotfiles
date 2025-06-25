587dd9f0dad8febfd975c7b92627caa8abdc9813
1337est <59178218+1337est@users.noreply.github.com>
2025-06-25 14:55:45 -0500

    typo?

--- configs/confighome/shellements/nvim/lua/plugins/colorizer.lua
+++ configs/confighome/shellements/nvim/lua/plugins/colorizer.lua
@@ -9,7 +9,7 @@ return {
             {
                 RGB      = true,         -- #FF0 hex codes
                 RRGGBB   = true,         -- #FF0000 hex codes
-                names    = false,        -- "Name" codes like Blue
+                names    = false,        -- "Name" codes like blue
                 RRGGBBAA = false,        -- #RRGGBBAA hex codes
                 rgb_fn   = false,        -- CSS rgb() and rgba() functions
                 hsl_fn   = false,        -- CSS hsl() and hsla() functions
